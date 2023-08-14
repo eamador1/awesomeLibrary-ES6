0f58ff610ac26b3e86b7e680a2d8dddc86954b7b
erwin amador <eamador0305@gmail.com>
2023-08-14 16:47:06 -0600

    refactor code and add time

---
+++ index.js
@@ -0,0 +1,94 @@
+import time from './modules/date.js';
+import spa from './modules/spa.js';
+
+const timeDisplay = document.querySelector('.dateTime');
+
+class Library {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('library')) || [];
+  }
+
+  addBook(title, author) {
+    const newBook = { title, author };
+    this.books.push(newBook);
+    this.updateLocalStorage();
+    this.displayBooks();
+  }
+
+  deleteBook(index) {
+    this.books.splice(index, 1);
+    this.updateLocalStorage();
+    this.displayBooks();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('library', JSON.stringify(this.books));
+  }
+
+  displayBooks() {
+    const $books = document.querySelector('.displayLibrary');
+    $books.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const $contBook = document.createElement('div');
+      const $title = document.createElement('h6');
+      const $by = document.createElement('h6');
+      const $author = document.createElement('h6');
+      const $delete = document.createElement('button');
+      $contBook.classList.add('cont-book');
+      $title.classList.add('titleBook');
+      $by.classList.add('by');
+      $author.classList.add('authorBook');
+      $delete.classList.add('delete');
+
+      $title.innerText = `"${book.title}"`;
+      $author.textContent = book.author;
+      $by.textContent = 'by';
+      $delete.innerHTML = 'Remove';
+
+      $contBook.appendChild($title);
+      $contBook.appendChild($by);
+      $contBook.appendChild($author);
+      $contBook.appendChild($delete);
+
+      $delete.addEventListener('click', () => this.deleteBook(index));
+
+      $books.appendChild($contBook);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const myLibrary = new Library();
+
+  function emptyForm() {
+    const $form = document.querySelector('.addForm');
+    $form.reset();
+  }
+
+  function addBookToLibrary() {
+    const $captureTitle = document.querySelector('#title');
+    const $captureAuthor = document.querySelector('#author');
+    myLibrary.addBook($captureTitle.value, $captureAuthor.value);
+    emptyForm();
+  }
+
+  const $form = document.querySelector('.addForm');
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+  });
+
+  const $books = document.querySelector('.displayLibrary');
+  $books.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete')) {
+      const bookIndex = Array
+        .from(event.target.parentNode.parentNode.children).indexOf(event.target.parentNode);
+      myLibrary.deleteBook(bookIndex);
+    }
+  });
+
+  myLibrary.displayBooks(); // Call the method to display books initially.
+});
+time(timeDisplay);
+spa();
